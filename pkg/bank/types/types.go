b50d55e54271df1d8ae1e91607938286174023fe
Aziz.Huseynov <huseynov.aziz@gmail.com>
2020-09-22 05:56:56 +0500

    Добавленны типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,50 @@
+package types
+
+
+// Category представлет собой категорию где был совершен платеж.
+type Category string
+
+
+// Payment is informed about payments.
+type Payment struct {
+	ID int
+	Amount Money
+
+	Category Category
+
+}
+
+
+//Money it is showed minimum units of money.
+type Money int64
+
+// Currency  is showed cod of valute.
+type Currency string
+
+// Cod of valyte
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN showed a number of card
+type PAN string
+
+// Card showed information about payment
+type Card struct{
+	ID  		int
+	PAN 		PAN
+	Balance  	Money
+	Currency  	Currency
+	Color  		string
+	Name  		string
+	Active 		bool
+	MinBalance	Money
+}
+// PaymentSource it doing somethink
+type PaymentSource struct {
+	Type string // 'card'
+	Number PAN //номер вида '5555 xxxx xxxx 2525'
+	Balance Money
+}
