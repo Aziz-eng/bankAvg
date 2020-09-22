b066449294fc85b255b66a6dc65b4015b775a703
Aziz.Huseynov <huseynov.aziz@gmail.com>
2020-09-22 08:19:41 +0500

    modify example

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -2,19 +2,21 @@ package stats
 
 import (
 	"fmt"
+
 	"github.com/Aziz-eng/bankAvg/pkg/bank/types"
 )
+
 // for
-func ExampleAvg()  {
-	payment := []types.Payment {
+func ExampleAvg() {
+	payment := []types.Payment{
 		{
-			ID: 1,
-			Amount: 100,
+			ID:       1,
+			Amount:   100,
 			Category: "Market",
 		},
 		{
-			ID: 2,
-			Amount: 400,
+			ID:       2,
+			Amount:   400,
 			Category: "Petrol",
 		},
 	}
